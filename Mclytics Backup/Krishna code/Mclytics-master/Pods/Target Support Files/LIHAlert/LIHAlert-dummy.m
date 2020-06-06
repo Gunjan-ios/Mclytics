65b71c90fd6599efeab0e95aced54ffe3239f9fe
Gunjan Raval <grawal72@gmail.com>
2020-06-06 17:53:31 +0530

    remove backup

--- Mclytics Backup/Krishna code/Mclytics-master/Pods/Target Support Files/LIHAlert/LIHAlert-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LIHAlert : NSObject
-@end
-@implementation PodsDummy_LIHAlert
-@end
--- Mclytics Backup/Krishna code/Mclytics-master/Pods/Target Support Files/Pods-Mclytics/Pods-Mclytics-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mclytics : NSObject
-@end
-@implementation PodsDummy_Pods_Mclytics
-@end
